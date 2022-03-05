1e7b45643b2b879c42abecb42ecfa68331b0fd39
Jesse Malinosky <jesse@malinoskj2.dev>
2022-03-05 13:30:52 -0500

    move existing Telescope launch mappings to Telescope specific mapping file

--- lua/jesse/mapping/plugin/telescope.lua
+++ lua/jesse/mapping/plugin/telescope.lua
@@ -1,4 +1,5 @@
 -- Telescope Mappings
+local builtin_present, builtin = pcall(require, "telescope.builtin")
 local util = require "jesse.core.util"
 
 local M = {}
@@ -11,5 +12,23 @@ M.mappings = {
     ["<C-v>"] = util.open_telescope_selection_in_new_instance,
   },
 }
+
+-- Launch mappings
+-- Fallback to find_files if not in git repo
+local git_or_find_files = function()
+  local opts = {}
+  local ok = pcall(builtin.git_files, opts)
+  if not ok then
+    builtin.find_files(opts)
+  end
+end
+
+vim.keymap.set("n", "<leader>f", function()
+  git_or_find_files()
+end, { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>g", function()
+  builtin.live_grep()
+end, { noremap = true, silent = true })
 
 return M
--- lua/jesse/plugin/config/telescope.lua
+++ lua/jesse/plugin/config/telescope.lua
@@ -1,11 +1,6 @@
 -- Telescope Configuration
 local telescope_present, telescope = pcall(require, "telescope")
-local builtin_present, builtin = pcall(require, "telescope.builtin")
 local telescope_mapping_config = require "jesse.mapping.plugin.telescope"
-
-if not telescope_present or not builtin_present then
-  return
-end
 
 -- Setup
 telescope.setup {
@@ -26,21 +21,3 @@ telescope.setup {
 
 -- Native FZY sorting
 telescope.load_extension "fzy_native"
-
--- Fallback to find_files if not in git repo
-local git_or_find_files = function()
-  local opts = {}
-  local ok = pcall(builtin.git_files, opts)
-  if not ok then
-    builtin.find_files(opts)
-  end
-end
-
--- Launch mappings
-vim.keymap.set("n", "<leader>f", function()
-  git_or_find_files()
-end, { noremap = true, silent = true })
-
-vim.keymap.set("n", "<leader>g", function()
-  builtin.live_grep()
-end, { noremap = true, silent = true })
