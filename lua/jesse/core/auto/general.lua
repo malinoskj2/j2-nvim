6995146be87ed555ec8931d8aaf460bb330efc3a
Jesse Malinosky <jesse@malinoskj2.dev>
2022-04-25 21:11:35 -0400

    disable bufwinter

--- lua/jesse/core/auto/general.lua
+++ lua/jesse/core/auto/general.lua
@@ -1,9 +1,9 @@
 -- General Auto Commands
 
 -- Disable for certain filetypes
-vim.cmd [[
-   autocmd BufEnter,BufRead,BufWinEnter,FileType,WinEnter * lua require("jesse.core.util").hide_status_line()
-]]
+-- vim.cmd [[
+--    autocmd BufEnter,BufRead,BufWinEnter,FileType,WinEnter * lua require("jesse.core.util").hide_status_line()
+-- ]]
 
 -- Set Spelling on when appropriate
 vim.cmd [[
