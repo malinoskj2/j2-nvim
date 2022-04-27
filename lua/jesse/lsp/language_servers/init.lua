603aa0396754068814329b38d99bf914df9e50fe
Jesse Malinosky <jesse@malinoskj2.dev>
2022-04-26 20:23:12 -0400

    add language server for nix

--- lua/jesse/lsp/language_servers/init.lua
+++ lua/jesse/lsp/language_servers/init.lua
@@ -1,6 +1,7 @@
 return {
   intelephense = require "jesse.lsp.language_servers.intelephense",
   jsonls = require "jesse.lsp.language_servers.jsonls",
+  rnix = require "jesse.lsp.language_servers.rnix",
   sumneko_lua = require "jesse.lsp.language_servers.sumneko_lua",
   tailwindcss = require "jesse.lsp.language_servers.tailwindcss",
   tsserver = require "jesse.lsp.language_servers.tsserver",
--- lua/jesse/lsp/languages/nix.lua
+++ lua/jesse/lsp/languages/nix.lua
@@ -1,5 +1,8 @@
 return {
   name = "Nix",
+  language_server = {
+    name = "rnix",
+  },
   null_language_servers = {
     "nixfmt",
   },
