325f6cacae30624abd8b6c184e0026108d29b7b1
Jesse Malinosky <jesse@malinoskj2.dev>
2022-08-13 13:10:42 -0400

    remove duplicate property

--- lua/jesse/lsp/language_servers/init.lua
+++ lua/jesse/lsp/language_servers/init.lua
@@ -1,5 +1,4 @@
 return {
-  intelephense = require "jesse.lsp.language_servers.intelephense",
   jsonls = require "jesse.lsp.language_servers.jsonls",
   rnix = require "jesse.lsp.language_servers.rnix",
   sumneko_lua = require "jesse.lsp.language_servers.sumneko_lua",
